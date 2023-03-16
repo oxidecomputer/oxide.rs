c88455ad0ec95c977e72a64912023282cca3be3c
Adam Leventhal <ahl@oxide.computer>
2023-03-15 20:03:03 -0700

    look for authentication info in OXIDE_HOST and OXIDE_TOKEN env variables (#22)

--- cli/src/context.rs
+++ cli/src/context.rs
@@ -16,11 +16,29 @@ pub struct Context {
 
 impl Context {
     pub fn new(config: Config) -> Result<Self> {
-        let Some((hostname, host)) = config.hosts.hosts.iter().next() else {
-            return Err(anyhow!("no authenticated hosts"));
+        let (host, token) = match (std::env::var("OXIDE_HOST"), std::env::var("OXIDE_TOKEN")) {
+            (Ok(host), Ok(token)) => (host, token),
+            (Ok(host), Err(_)) => {
+                let Some(host_entry) = config.hosts.get(&host) else {
+                    return Err(anyhow!("host {} not found", host));
+                };
+                (host, host_entry.token.clone())
+            }
+            (Err(_), Ok(token)) => {
+                let Some((host, _)) = config.hosts.hosts.iter().next() else {
+                    return Err(anyhow!("no authenticated hosts"));
+                };
+                (host.clone(), token)
+            }
+            (Err(_), Err(_)) => {
+                let Some((host, host_entry)) = config.hosts.hosts.iter().next() else {
+                    return Err(anyhow!("no authenticated hosts"));
+                };
+                (host.clone(), host_entry.token.clone())
+            }
         };
 
-        let auth = format!("Bearer {}", &host.token);
+        let auth = format!("Bearer {}", token);
         let mut auth_value = reqwest::header::HeaderValue::from_str(&auth)?;
         auth_value.set_sensitive(true);
 
@@ -35,7 +53,7 @@ impl Context {
             )
             .build()
             .unwrap();
-        let client = oxide_api::Client::new_with_client(hostname, rclient);
+        let client = oxide_api::Client::new_with_client(&host, rclient);
 
         Ok(Self { client, config })
     }
