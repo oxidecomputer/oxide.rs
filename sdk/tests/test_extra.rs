44881dd0350aa96968dd8d43ed1e0b68c7002ef1
Will Chandler <will@oxidecomputer.com>
2024-12-16 09:35:24 -0500

    Add copyright info

--- sdk/tests/test_extra.rs
+++ sdk/tests/test_extra.rs
@@ -1,3 +1,9 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+// Copyright 2024 Oxide Computer Company
+
 use httpmock::MockServer;
 use oxide::extras::disk::types::DiskInfo;
 use oxide::extras::ClientExtraDiskExt;
