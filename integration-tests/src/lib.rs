a8a246031dd6a82ebcb0ca7f4f6d45edc4e69c87
Adam H. Leventhal <ahl@oxide.computer>
2024-12-17 21:38:41 -0800

    remove circular dependency between oxide and oxide-httpmock

---
+++ integration-tests/src/lib.rs
@@ -0,0 +1,8 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+// Copyright 2024 Oxide Computer Company
+
+//! This crate exists only for stand-alone integration tests that act as SDK
+//! consumers.
