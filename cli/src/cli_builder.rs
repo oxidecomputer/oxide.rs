f61c06f6b78a2defdbdeae77a3c661924baa8089
David Crespo <david.crespo@oxidecomputer.com>
2026-03-26 09:09:30 -0500

    copyright years

--- cli/src/cli_builder.rs
+++ cli/src/cli_builder.rs
@@ -2,7 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-// Copyright 2025 Oxide Computer Company
+// Copyright 2026 Oxide Computer Company
 
 use std::{any::TypeId, collections::BTreeMap, marker::PhantomData, net::IpAddr, path::PathBuf};
 
--- cli/src/cmd_docs.rs
+++ cli/src/cmd_docs.rs
@@ -2,7 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-// Copyright 2024 Oxide Computer Company
+// Copyright 2026 Oxide Computer Company
 
 use crate::cli_builder::OperationId;
 use crate::context::Context;
