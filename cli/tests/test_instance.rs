4f6a0e886974bc850f5f52064a3c7d84db161fcf
Adam Leventhal <ahl@oxide.computer>
2026-09-23 11:06:21 -0700

    Add --ip-pool and --ip-version to instance from-image (#1553)

--- cli/tests/test_instance.rs
+++ cli/tests/test_instance.rs
@@ -2,10 +2,12 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-// Copyright 2023 Oxide Computer Company
+// Copyright 2026 Oxide Computer Company
 
 use httpmock::prelude::*;
-use oxide::types::{InstanceSerialConsoleData, NameOrId};
+use oxide::types::{
+    ExternalIpCreate, InstanceCreate, InstanceDiskAttachment, InstanceSerialConsoleData, NameOrId,
+};
 use oxide_httpmock::MockServerExt;
 use predicates::prelude::predicate;
 use rand::SeedableRng;
@@ -126,4 +128,89 @@ fn test_instance_serial_console() {
         .stderr(pred);
 
     mock.assert();
+}
+
+#[test]
+fn test_instance_from_image() {
+    let mut src = rand::rngs::SmallRng::seed_from_u64(42);
+    let server = MockServer::start();
+
+    let image_response = JsonMock::mock_value(&mut src).unwrap();
+    let image_view = server.image_view(|when, then| {
+        when.image(&"blank-i-n-g-o".parse().unwrap())
+            .project(Some(&"projname".parse().unwrap()));
+        then.ok(&image_response);
+    });
+
+    let instance_response = oxide::types::Instance {
+        name: "bingo".parse().unwrap(),
+        description: "there was a farmer who had a dog".to_string(),
+        hostname: "bingo".to_string(),
+        memory: (100 * 1024 * 1024 * 1024).into(),
+        ncpus: 6.into(),
+        run_state: oxide::types::InstanceState::Creating,
+        ..JsonMock::mock_value(&mut src).unwrap()
+    };
+    let instance_create = server.instance_create(|when, then| {
+        when.project(&"projname".parse().unwrap()).body(
+            &InstanceCreate::builder()
+                .name(instance_response.name.clone())
+                .description(instance_response.description.clone())
+                .boot_disk(Some(InstanceDiskAttachment::Create {
+                    description: "bingo disk".to_string(),
+                    disk_backend: oxide::types::DiskBackend::Distributed(
+                        oxide::types::DiskSource::Image {
+                            image_id: image_response.id,
+                            read_only: false,
+                        },
+                    ),
+                    name: "bingo-disk".parse().unwrap(),
+                    size: (1024 * 1024 * 1024 * 1024).into(),
+                }))
+                .external_ips(vec![ExternalIpCreate::Ephemeral {
+                    pool_selector: oxide::types::PoolSelector::Auto {
+                        ip_version: Some(oxide::types::IpVersion::V6),
+                    },
+                }])
+                .hostname(instance_response.hostname.clone())
+                .memory(instance_response.memory.clone())
+                .ncpus(instance_response.ncpus.clone())
+                .start(false)
+                .try_into()
+                .unwrap(),
+        );
+        then.created(&instance_response);
+    });
+
+    assert_cmd::cargo::cargo_bin_cmd!("oxide")
+        .env("OXIDE_HOST", server.url(""))
+        .env("OXIDE_TOKEN", "fake-token")
+        .arg("instance")
+        .arg("from-image")
+        .arg("--name")
+        .arg("bingo")
+        .arg("--project")
+        .arg("projname")
+        .arg("--description")
+        .arg("there was a farmer who had a dog")
+        .arg("--hostname")
+        .arg("bingo")
+        .arg("--memory")
+        .arg("100g")
+        .arg("--ncpus")
+        .arg("6")
+        .arg("--image")
+        .arg("blank-i-n-g-o")
+        .arg("--size")
+        .arg("1t")
+        .arg("--ip-version")
+        .arg("v6")
+        .assert()
+        .success()
+        .stdout(expectorate::eq_file_or_panic(
+            "tests/data/test_instance_from_image.stdout",
+        ));
+
+    image_view.assert();
+    instance_create.assert();
 }
